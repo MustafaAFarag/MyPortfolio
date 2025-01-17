118607f44933dc2fdc7f8699445260f4a9d932ba
Mustafa Ashraf <mustafa.ashraf.saad@gmail.com>
2025-01-17 17:36:28 +0200

    SVG changed

--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import FloatingNavbar from "@/components/ui/FloatingNavbar";
 const Home = () => {
   return (
     <>
-      {/* <div className="sticky bg-red-500 w-full">Test</div> */}
       <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
         <div className=" ">
           <FloatingNavbar />
